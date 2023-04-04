0626bd119daf16cf16c4ab8ff5cde0cd4b8e67d3
Joonas Tiala <joonas.tiala@gmail.com>
2023-04-05 00:49:33 +0300

    Improve building

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
-      name: "themeless-ui",
-      formats: ["es"],
+      name: "ThemelessUI",
+      fileName: "themeless-ui",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
