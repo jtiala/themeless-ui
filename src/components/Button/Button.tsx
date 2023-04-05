fc5f09e895c7531393c9af8f11f522020ea3d6b1
Joonas Tiala <joonas.tiala@gmail.com>
2023-04-05 23:33:57 +0300

    Add Storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,22 @@
 import { HTMLAttributes } from "react";
 
-export type ButtonProps = HTMLAttributes<HTMLButtonElement>;
+type HTMLButtonProps = HTMLAttributes<HTMLButtonElement>;
 
+export interface ButtonProps {
+  /**
+   * Button content.
+   */
+  children: HTMLButtonProps["children"];
+
+  /**
+   * Callback to call when the button is clicked.
+   */
+  onClick?: HTMLButtonProps["onClick"];
+}
+
+/**
+ * A clickable UI element commonly used to trigger actions, such as submitting a form.
+ */
 export function Button(props: ButtonProps) {
   return <button {...props} />;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-export { Button } from "./components/Button/Button";
-export type { ButtonProps } from "./components/Button/Button";
+export * from "./components/Button";
