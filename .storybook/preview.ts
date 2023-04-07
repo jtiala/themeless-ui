97579bc4f628d25b200e76a2c55bf1f25b1ffc57
Joonas Tiala <joonas.tiala@gmail.com>
2023-04-07 23:47:53 +0300

    Sort stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,11 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: ["Introduction", "Typography", "Form"],
+      },
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
