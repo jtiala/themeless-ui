ccc84dd536f1977298aea11935ac471894da97c1
Joonas Tiala <joonas.tiala@gmail.com>
2023-07-24 22:59:58 +0300

    feat: add blockquote component

---
+++ packages/react/src/components/Blockquote/Blockquote.tsx
@@ -0,0 +1,61 @@
+import { CommonComponentProps, cn } from "@themeless-ui/utils";
+import { ReactNode } from "react";
+
+export type BlockquoteProps = {
+  /**
+   * A URI of the source of the information quoted.
+   */
+  cite?: string;
+
+  /**
+   * Author of the quotation.
+   */
+  author?: string;
+
+  /**
+   * Source of the quotation.
+   */
+  source?: string;
+
+  /**
+   * Quotation content.
+   */
+  children?: ReactNode;
+} & CommonComponentProps;
+
+const blockquoteClassName = cn("blockquote");
+const quoteClassName = cn("blockquote-quote");
+const authorClassName = cn("blockquote-author");
+const sourceClassName = cn("blockquote-source");
+
+/**
+ * Display an extended quotation.
+ */
+export function Blockquote({
+  cite,
+  author,
+  source,
+  children,
+  testId,
+}: BlockquoteProps) {
+  return (
+    <blockquote
+      cite={cite}
+      className={blockquoteClassName}
+      data-testid={testId}
+    >
+      <div className={quoteClassName}>{children}</div>
+      {(author || source) && (
+        <footer>
+          {author && <span className={authorClassName}>&mdash;{author}</span>}
+          {author && source && <span>, </span>}
+          {source && (
+            <cite key="source" className={sourceClassName}>
+              {source}
+            </cite>
+          )}
+        </footer>
+      )}
+    </blockquote>
+  );
+}
--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,3 +1,4 @@
+export * from "./Blockquote";
 export * from "./Button";
 export * from "./Heading";
 export * from "./Paragraph";
