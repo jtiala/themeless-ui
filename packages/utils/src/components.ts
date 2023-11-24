b3a6a5cd42301716d0ab8ebeddfdf970596a4010
Joonas Tiala <joonas.tiala@gmail.com>
2023-11-24 11:54:39 +0200

    docs: fix typo

--- packages/utils/src/components.ts
+++ packages/utils/src/components.ts
@@ -5,7 +5,7 @@ export type CommonComponentProps = {
   id?: string;
 
   /**
-   * Provide `data-testid` attribute for the component to be used with testing tools.
+   * Provides `data-testid` attribute for the component to be used with testing tools.
    * While the `testId` is a useful way for targeting the component, it should be used as a last resort.
    * Whenever possible, use other attributes or selectors that have semantic meaning, such as `id` or `name`.
    */
