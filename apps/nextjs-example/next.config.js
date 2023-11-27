f85fc46dee2568b1019a87c3f50ba4b9b155c0f2
Joonas Tiala <joonas.tiala@gmail.com>
2023-11-27 13:26:23 +0200

    chore: ignore webpack infrastructure warnings as a temporary solution

--- apps/nextjs-example/next.config.js
+++ apps/nextjs-example/next.config.js
@@ -1,6 +1,16 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    // TODO: Remove when https://github.com/vercel/next.js/issues/33693 gets resolved
+    config.infrastructureLogging = {
+      level: "error",
+    };
+
+    return config;
+  },
+};
 
 export default withContentlayer(nextConfig);
