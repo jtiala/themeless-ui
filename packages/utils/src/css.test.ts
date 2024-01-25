e94195d474718390be13f462e92f90f1b30c9f51
Joonas Tiala <joonas.tiala@gmail.com>
2024-01-25 14:27:40 +0200

    feat: add util function for generating css class name strings conditionally

--- packages/utils/src/css.test.ts
+++ packages/utils/src/css.test.ts
@@ -1,9 +1,25 @@
 import { describe, expect, it } from "vitest";
-import { CSS_PREFIX, cn } from "./css";
+import { CSS_PREFIX, cn, cns } from "./css";
 
 describe("cn", async () => {
   it("should generate scoped class name", () => {
-    const className = cn("foo");
-    expect(className).toBe(`${CSS_PREFIX}foo`);
+    expect(cn("foo")).toBe(`${CSS_PREFIX}foo`);
+  });
+});
+
+describe("cns", async () => {
+  it("should generate correct set of class names", () => {
+    expect(cns(false)).toBe("");
+    expect(cns(undefined)).toBe("");
+    expect(cns(null)).toBe("");
+    expect(cns("a")).toBe("a");
+    expect(cns("a", "b")).toBe("a b");
+    expect(cns(["a", "b"])).toBe("a b");
+    expect(cns("a", ["b", "c"], "d")).toBe("a b c d");
+    expect(cns({ a: true, b: false, c: true })).toBe("a c");
+    expect(cns("a", { b: false, c: true }, "d")).toBe("a c d");
+    expect(
+      cns({ a: true, b: false, c: true }, undefined, "d", false, ["e f"], null),
+    ).toBe("a c d e f");
   });
 });
--- packages/utils/src/css.ts
+++ packages/utils/src/css.ts
@@ -1,5 +1,7 @@
+import { isNonEmpty, isNullish } from "./predicates";
+
 /**
- * Prefix to be used when generating CSS class names.
+ * Prefix to be used when generating scoped CSS class names.
  */
 export const CSS_PREFIX = "tui__";
 
@@ -8,4 +10,38 @@ export const CSS_PREFIX = "tui__";
  */
 export function cn(className: string) {
   return `${CSS_PREFIX}${className}`;
+}
+
+type CnsArgument =
+  | undefined
+  | null
+  | false
+  | string
+  | { [key: string]: boolean }
+  | CnsArgument[];
+
+/**
+ * Construct CSS class name string conditionally.
+ */
+export function cns(...args: CnsArgument[]) {
+  return args.reduce<string>((acc, arg): string => {
+    if (isNullish(arg) || arg === false) {
+      return acc;
+    } else if (Array.isArray(arg)) {
+      return [acc, arg.map((a) => cns(a)).join(" ")]
+        .filter(isNonEmpty)
+        .join(" ");
+    } else if (typeof arg === "object") {
+      return [
+        acc,
+        Object.entries(arg).reduce<string>((a, [k, v]): string => {
+          return v ? [a, k].filter(isNonEmpty).join(" ") : a;
+        }, ""),
+      ]
+        .filter(isNonEmpty)
+        .join(" ");
+    }
+
+    return [acc, arg].filter(isNonEmpty).join(" ");
+  }, "");
 }
